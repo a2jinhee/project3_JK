0beee991bc69347fbabf1b3c8c03d400df7c77fd
Jinhee Kim <a2jinhee@gmail.com>
2024-06-15 17:50:35 +0900

    RTL: update

--- MME/RTL/DMA_ENGINE.sv
+++ MME/RTL/DMA_ENGINE.sv
@@ -165,6 +165,7 @@ module DMA_ENGINE
                 axi_w_if.wvalid = 1;
                 axi_w_if.wid = 0;
                 axi_w_if.wstrb = 'hf;
+                axi_b_if.bready = 1;
 
                 if (axi_w_if.wlast)
                     state_n = DONE;
@@ -173,7 +174,6 @@ module DMA_ENGINE
                 // B CHANNEL
                 // - output: bready
                 // - input: bvalid, bid, bresp
-                axi_b_if.bready = 1;
 
                 if (axi_b_if.bvalid)
                     state_n = IDLE;
