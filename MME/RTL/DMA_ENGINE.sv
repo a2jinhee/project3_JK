502d73c0d6c3db9f0b8652621a26cbf6d398b24b
Jinhee Kim <a2jinhee@gmail.com>
2024-06-15 10:08:02 +0900

    RTL: update

--- MME/RTL/DMA_ENGINE.sv
+++ MME/RTL/DMA_ENGINE.sv
@@ -84,14 +84,38 @@ module DMA_ENGINE
                     state_n = ADDR_A;
             end
             ADDR_A: begin
+                // AR CHANNEL
+                // - output: arvalid, arid, araddr, arlen, arsize, arburst
+                // - input: arready
+                axi_ar_if.arvalid = 1;
+                axi_ar_if.arlen = 15;
+                axi_ar_if.arsize = 4;
+                axi_ar_if.arburst = 1;
+                axi_ar_if.arid = 0; 
+                axi_ar_if.araddr = mat_a_addr_i; 
+
                 if (!axi_ar_if.arvalid)
                     state_n = ADDR_B;
             end
             ADDR_B: begin
+                // AR CHANNEL
+                // - output: arvalid, arid, araddr, arlen, arsize, arburst
+                // - input: arready
+                axi_ar_if.arvalid = 1;
+                axi_ar_if.arlen = 15;
+                axi_ar_if.arsize = 4;
+                axi_ar_if.arburst = 1;
+                axi_ar_if.arid = 1;
+                axi_ar_if.araddr = mat_b_addr_i; 
+
                 if (!axi_ar_if.arvalid)
                     state_n = LOAD;
             end
             LOAD: begin
+                // R CHANNEL
+                // - output: rready
+                // - input: rvalid, rid, rdata, rlast
+                axi_r_if.rready = 1;
                 if ((buf_a_addr / 4 == mat_width_i) && (buf_b_addr / 4 == mat_width_i))
                     state_n = WAIT_MM;
             end
@@ -100,10 +124,23 @@ module DMA_ENGINE
                     state_n = WRITE_C;
             end
             ADDR_C: begin
-                if (axi_aw_if.awready)
+                // AW CHANNEL
+                // - output: awvalid, awid, awaddr, awlen, awsize, awburst
+                // - input: awready
+                axi_aw_if.awvalid = 1;
+                axi_aw_if.awlen = 15;
+                axi_aw_if.awsize = 4;
+                axi_aw_if.awburst = 1;
+                axi_aw_if.awaddr <= mat_c_addr_i; 
+
+                if (!axi_ar_if.arvalid)
                     state_n = WRITE_C;
             end
             WRITE_C: begin
+                // W CHANNEL
+                // - output: wvalid, wid, wdata, wlast
+                // - input: wready
+                axi_w_if.wvalid = 1;
                 if (done_o)
                     state_n = IDLE;
             end
@@ -127,38 +164,7 @@ module DMA_ENGINE
         end else begin
             case (state)
 
-                ADDR_A: begin
-                    // AR CHANNEL
-                    // - output: arvalid, arid, araddr, arlen, arsize, arburst
-                    // - input: arready
-                    axi_ar_if.arvalid <= 1; 
-
-                    axi_ar_if.arlen <= 15;
-                    axi_ar_if.arsize <= 4;
-                    axi_ar_if.arburst <= 1;
-                    axi_ar_if.arid <= 0; 
-                    axi_ar_if.araddr <= mat_a_addr_i; 
-
-                end
-                ADDR_B: begin
-                    // AR CHANNEL
-                    // - output: arvalid, arid, araddr, arlen, arsize, arburst
-                    // - input: arready
-                    axi_ar_if.arvalid <= 1;
-                    
-                    axi_ar_if.arlen <= 15;
-                    axi_ar_if.arsize <= 4;
-                    axi_ar_if.arburst <= 1;
-                    axi_ar_if.arid <= 1;
-                    axi_ar_if.araddr <= mat_b_addr_i; 
-                end
-
-                LOAD: begin
-                    // R CHANNEL
-                    // - output: rready
-                    // - input: rvalid, rid, rdata, rlast
-                    axi_r_if.rready <= 1;
-
+                LOAD: begin                    
                     // buffer A - handshake && id
                     if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 0) begin
                         buf_a_addr <= 0;
@@ -195,29 +201,9 @@ module DMA_ENGINE
                         buf_b_addr <= buf_b_addr + 4;
                     end
                     
-
-                end
-
-                ADDR_C: begin
-                    // AW CHANNEL
-                    // - output: awvalid, awid, awaddr, awlen, awsize, awburst
-                    // - input: awready
-                    axi_aw_if.awvalid <= 1;
-
-                    axi_aw_if.awlen <= 15;
-                    axi_aw_if.awsize <= 4;
-                    axi_aw_if.awburst <= 1;
-
-                    axi_aw_if.awaddr <= mat_c_addr_i; // byte address
-
                 end
 
                 WRITE_C: begin
-                    // W CHANNEL
-                    // - output: wvalid, wid, wdata, wlast
-                    // - input: wready
-                    axi_w_if.wvalid <= 1;
-
                     if (axi_w_if.wready && axi_w_if.wvalid) begin
                         axi_w_if.wdata <= accum_i[count_c / 4][count_c % 4];
                         count_c <= count_c + 1;
