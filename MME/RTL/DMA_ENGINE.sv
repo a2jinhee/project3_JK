205dedbccada65c8ee654937f1c3d07b16200656
Jinhee Kim <a2jinhee@gmail.com>
2024-06-15 16:55:11 +0900

    RTL: update

--- MME/RTL/DMA_ENGINE.sv
+++ MME/RTL/DMA_ENGINE.sv
@@ -206,7 +206,8 @@ module DMA_ENGINE
                     if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 0) begin
                         buf_a_wbyteenable_o <= 'hffff;
                         buf_a_addr <= buf_a_addr;
-                        buf_a_data <= (buf_a_data << 32) | axi_r_if.rdata;
+                        // buf_a_data <= (buf_a_data << 32) | axi_r_if.rdata;
+                        buf_a_data <= (buf_a_data >> 32) | (axi_r_if.rdata << 96);
                         count_a <= count_a + 1;
                     end
 
@@ -222,7 +223,8 @@ module DMA_ENGINE
                     if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 1) begin
                         buf_b_wbyteenable_o <= 'hffff;
                         buf_b_addr <= buf_b_addr;
-                        buf_b_data <= (buf_b_data << 32) | axi_r_if.rdata;
+                        // buf_b_data <= (buf_b_data << 32) | axi_r_if.rdata;
+                        buf_b_data <= (buf_b_data >> 32) | (axi_r_if.rdata << 96);
                         count_b <= count_b + 1;
                     end
 
