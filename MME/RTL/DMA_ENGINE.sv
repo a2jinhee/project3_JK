3f130bd0dac59d48bfbdcbee9677522a34b05f7f
Jinhee Kim <a2jinhee@gmail.com>
2024-06-19 21:04:41 +0900

    RTL: update

--- MME/RTL/DMA_ENGINE.sv
+++ MME/RTL/DMA_ENGINE.sv
@@ -58,7 +58,7 @@ module DMA_ENGINE
 
     reg [2:0] state, state_n;
     reg [BUF_DW-1:0] buf_a_data, buf_b_data, buf_a_data_n, buf_b_data_n;
-    reg [BUF_AW-1:0] buf_a_addr, buf_b_addr;
+    reg [BUF_AW-1:0] buf_a_addr, buf_b_addr, buf_a_addr_n, buf_b_addr_n;
     reg [2:0] count_a, count_b, count_a_n, count_b_n;
     reg [4:0] count_c, count_c_n; 
     reg [1:0] burst_a, burst_b; 
@@ -78,12 +78,14 @@ module DMA_ENGINE
             count_c <= 0;
             count_a <= 0; count_b <= 0;
             buf_a_data <= 0; buf_b_data <= 0;
+            buf_a_addr <= 0; buf_b_addr <= 0;
         end else begin
             state <= state_n;
             burst_a <= burst_a_n; burst_b <= burst_b_n;
             count_c <= count_c_n;
             count_a <= count_a_n; count_b <= count_b_n;
             buf_a_data <= buf_a_data_n; buf_b_data <= buf_b_data_n;
+            buf_a_addr <= buf_a_addr_n; buf_b_addr <= buf_b_addr_n;
         end
         
     always_comb begin 
@@ -92,6 +94,7 @@ module DMA_ENGINE
         count_c_n = count_c;
         count_a_n = count_a; count_b_n = count_b;
         buf_a_data_n = buf_a_data; buf_b_data_n = buf_b_data;
+        buf_a_addr_n = buf_a_addr; buf_b_addr_n = buf_b_addr;
         
         // AXI interface AR channel
         axi_ar_if.arlen = 15; axi_ar_if.arsize = 4; axi_ar_if.arburst = 1;
@@ -112,7 +115,7 @@ module DMA_ENGINE
         done_o = 1;
         buf_a_wbyteenable_o = 'hffff;
         buf_b_wbyteenable_o = 'hffff;
-        
+
         buf_a_wren_o = 0; buf_b_wren_o = 0;
 
         case (state)
@@ -179,6 +182,9 @@ module DMA_ENGINE
                     buf_a_wren_o = 1;
                 end
 
+                if (buf_a_wren_o)
+                    buf_a_addr_n = buf_a_addr + 1;
+
                 if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 1) begin
                     buf_b_data_n <= (buf_b_data << 32) | axi_r_if.rdata;
                     count_b_n = count_b + 1;
@@ -188,6 +194,9 @@ module DMA_ENGINE
                     count_b_n = 0;
                     buf_b_wren_o = 1;
                 end
+
+                if (buf_b_wren_o)
+                    buf_b_addr_n = buf_b_addr + 1;
 
 
                 if ((buf_a_addr == mat_width_i) && (buf_b_addr == mat_width_i))
@@ -265,7 +274,7 @@ module DMA_ENGINE
 
         if (!rst_n) begin
 
-            buf_a_addr <= 0; buf_b_addr <= 0;
+            // buf_a_addr <= 0; buf_b_addr <= 0;
             mm_start_o <= 0;
 
         end else begin
@@ -277,29 +286,29 @@ module DMA_ENGINE
                     // set buffer write enable to 1 every 128b=16B
 
                     // buffer A - handshake && id
-                    if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 0) begin
-                        buf_a_addr <= buf_a_addr;
+                    // if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 0) begin
+                    //     buf_a_addr <= buf_a_addr;
                         
-                    end
+                    // end
 
                     // if (count_a == 3) begin
                     //     buf_a_wren_o <= 1;
                     // end
 
-                    if (buf_a_wren_o)
-                        buf_a_addr <= buf_a_addr + 1;
+                    // if (buf_a_wren_o)
+                    //     buf_a_addr <= buf_a_addr + 1;
                     
                     // buffer B - handshake && id
-                    if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 1) begin
-                        buf_b_addr <= buf_b_addr;
-                    end
+                    // if (axi_r_if.rready && axi_r_if.rvalid && axi_r_if.rid == 1) begin
+                    //     buf_b_addr <= buf_b_addr;
+                    // end
 
                     // if (count_b == 3) begin
                     //     buf_b_wren_o <= 1;
                     // end
 
-                    if (buf_b_wren_o)
-                        buf_b_addr <= buf_b_addr + 1;
+                    // if (buf_b_wren_o)
+                    //     buf_b_addr <= buf_b_addr + 1;
                     
                     if ((buf_a_addr == mat_width_i) && (buf_b_addr == mat_width_i))
                         mm_start_o <= 1;
